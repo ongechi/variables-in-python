a25b8e908d393fa5d4f4cf61611399177a031f09
ongechi <77966899+ongechi@users.noreply.github.com>
2021-01-28 00:33:33 +0300

    Add files via upload

---
+++ assigning.py
@@ -0,0 +1,10 @@
+# Assigning a single value to multiple values
+x=y=z=15
+print(x)
+print(y)
+print(z)
+#assigning a different values to multiple variables
+a,b,c=1,20.2,"python programming"
+print(a)
+print(b)
+print(c)
